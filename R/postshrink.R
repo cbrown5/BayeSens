d6ddabe541913542fa978fbf58c47e2d832aeda0
cbrown5 <christo.j.brown@gmail.com>
2016-10-05 11:01:18 +1000

    fix postsrhink

--- R/postshrink.R
+++ R/postshrink.R
@@ -24,24 +24,27 @@
 #'straightfoward). For complex models the MLE may be estiamted using data
 #'cloning, see \code{\link{dataclone}}.
 #'
-#' Shrink values can occaisionally be >1. Values >1 occur when
+#' Shrink values can occaisionally be >1 or <0. Values <0 occur when
 #'the posterior parameter estimate has moved in the opposite direction from
-#'the prior than the MLE.  Values >1 can occur if (1) your MLE estimate
+#'the prior than the MLE.
+#' Values >1 occur when the MLE is closer to the prior estimate than
+#' the posterior.
+#' Values not in 0-1 can occur if (1) your MLE estimate
 #'is inaccurate, (2) your posterior is multi-model, or (3) your posterior
 #'estimate is constrained by other parameters. If (1) then try other methods
 #'for obtaining an MLE or increase replication if using data cloning.
 #'If (2) or (3) posterior shrink may be inappropriate for your model, because
-#'the posterior shrink cannot be characterised by a simple univariate measure. 
+#'the posterior shrink cannot be characterised by a simple univariate measure.
 #'
 #' See: Berger JO (1985) Statistical Decision Theory and Bayesian Analysis,
 #' Second Edition, Springer, New York.
 #'
-#' Thanks to Ed Boone for suggesting this one.  
+#' Thanks to Ed Boone for suggesting this one.
 #'
 #' @author Christopher J. Brown christo.j.brown@gmail.com
 #' @rdname postshrink
 #' @export
 
 postshrink <- function(thetaprior, thetapost, thetaMLE){
-		(theta_post - theta_prior)/(theta_MLE - theta_prior)
+		(thetapost - thetaprior)/(thetaMLE - thetaprior)
 		}
